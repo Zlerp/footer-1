22d4db265c02a2d757d966a2e2d89999d9ed92f9
Zlerp <zhpdeals@gmail.com>
2016-02-05 02:42:08 -0500

    add logo click to scroll up animation js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
+
+  killScroll = function() {
+    return false;
+  };
+
   $("#scroll").click(function() {
     $('html, body').animate({scrollTop:
       $("#footerContainer").offset().top}, 1000, 'easeInOutCirc');
-});
+  });
+  $(".logo").find('h2').find('a').click(function() {
+    $('html, body').animate({scrollTop:
+      $(".top").offset().top}, 1000, 'easeInOutCirc');
+  });
 });
